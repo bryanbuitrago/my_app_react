fb48d56c5dce00b1eedd6307df676dd9b36f0f8e
bryan buitrago <bryanbuitrago86@gmail.com>
2017-08-05 16:25:19 -0400

    succesful react app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/Header';
 
 class App extends Component {
   render() {
+    this.title = 'HELLO WORLD!!!';
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Header title={this.title} />
     );
   }
 }
